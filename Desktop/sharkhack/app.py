ee1cfd517418017a586709778b00d47c6e37309d
Kevin Kupeli <kkupeli@bu.edu>
2025-04-06 00:38:26 -0400

    fixed all bugs , currently working website

--- Desktop/sharkhack/app.py
+++ Desktop/sharkhack/app.py
@@ -6,6 +6,15 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('Home/index.html')  # Adjust the path if necessary
+
+# filepath: /Users/kevinkupeli/Desktop/shark-hacks-study-website-2/Desktop/sharkhack/app.py
+@app.route('/schedule')
+def schedule():
+    return render_template('Schedule/index.html')
+
 @app.route('/get-data')
 def get_data():
     data = {"message": "Hello from the Python backend!"}
